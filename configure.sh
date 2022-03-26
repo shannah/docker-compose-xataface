28b8f3f26ee432e351a50093ced9a28687ae1e9d
Steve Hannah <steve@weblite.ca>
2022-03-26 08:06:58 -0700

    Added configure.sh script that installs the Xataface application.

---
+++ configure.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd "$SCRIPTPATH"
+if [ ! -f ".env" ]; then
+  echo "Please copy the sample.env to .env then run this command again."
+  exit 1
+fi
+source .env
+
+docker-compose up -d
+if [ ! -d "www/admin" ]; then
+  docker-compose exec webserver composer create-project "$COMPOSER_TEMPLATE_NAME" "admin"
+fi
+cat << EOF > www/admin/conf.db.ini.php
+;<?php exit;
+[_database]
+    host = "database"
+    user = "$MYSQL_USER"
+    password = "$MYSQL_PASSWORD"
+    name = "$MYSQL_DATABASE"
+    driver=mysqli
+EOF
+
+
+
+
+
