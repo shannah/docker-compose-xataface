8745fb2dfbf368ad75f843302741a02ff165908c
Steve Hannah <steve@weblite.ca>
2022-05-08 11:55:57 -0700

    Another attempt to fix CI

--- configure.sh
+++ configure.sh
@@ -11,12 +11,12 @@ source .env
 
 chmod 0755 bin/start bin/stop bin/phpmyadmin
 
-
+echo "" | docker-compose up -d
 if [ ! -d "www/admin" ]; then
   # IMPORTANT: Need to pipe into docker-compose run so it doesn't eat the stdin with 
   # the remainder of the bash script
   # See https://www.reddit.com/r/bash/comments/u8o8y9/comment/i5m9q9g/?utm_source=share&utm_medium=web2x&context=3
-  echo "" | docker-compose run  webserver composer -n create-project "$COMPOSER_TEMPLATE_NAME" "admin"
+  echo "" | docker-compose exec -it  webserver composer -n create-project "$COMPOSER_TEMPLATE_NAME" "admin"
 fi
 if [ -f "$SCRIPTPATH/www/admin/configure.env" ]; then
     source "$SCRIPTPATH/www/admin/configure.env"
